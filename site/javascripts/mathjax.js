db416ed047a7bdd503187dc03bd6d51d4cbbc5a9
Abiesjqq <1508729918@qq.com>
2025-06-27 12:15:10 +0800

    add mathjax and C notes

---
+++ site/javascripts/mathjax.js
@@ -0,0 +1,26 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"],
+    ],
+    processEscapes: true,
+    processEnvironments: true,
+    tags: "ams",
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "math|arithmatex",
+  },
+};
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise();
+});
